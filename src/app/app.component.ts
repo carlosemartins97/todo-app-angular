b0773d1233a23f2d2970864875f75b016b5f888d
Carlos Martins <carlosemartins97@gmail.com>
2021-08-16 16:21:48 -0300

    feat: save tasks on local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   tasks: Array<string> = [];
 
+  ngOnInit() {
+    const savedTasks = localStorage.getItem('@todo-app');
+    if(savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    } else {
+      this.tasks = [];
+    }
+  }
+
   GetTasks(data: string) {
     this.tasks.push(data);
+    localStorage.setItem("@todo-app", JSON.stringify(this.tasks));
   }
   
   onDeleteTask(index: number) {
     this.tasks.splice(index, 1);
+    localStorage.setItem("@todo-app", JSON.stringify(this.tasks));
   }
 }
