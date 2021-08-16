e1b4e2846f84048a7a06b40fc96d104e34b28a9e
Carlos Martins <carlosemartins97@gmail.com>
2021-08-16 11:13:01 -0300

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputWrapperComponent } from './input-wrapper/input-wrapper.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-list/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputWrapperComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
